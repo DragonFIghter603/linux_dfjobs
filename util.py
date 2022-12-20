d2c12be8c5608e50cd2546a3edc2dc8af3d894d8
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2022-12-20 20:41:11 +0100

    Create util.py

---
+++ util.py
@@ -0,0 +1,21 @@
+class Job:
+    def __init__(self, path, enabled):
+        self.path = path
+        self.enabled = enabled
+
+    def __str__(self):
+        return f'{self.path}, {self.enabled}'
+
+
+def load_jobs():
+    with open('~/tools/dfjobs/jobfile.txt') as jobfile:
+        jobs = {}
+        for line in jobfile.readlines():
+            name, path, enabled = line.split(',')
+            jobs[name.strip()] = Job(path.strip(), enabled.strip() == 'True')
+        return jobs
+
+
+def save_jobs(jobs):
+    with open('~/tools/dfjobs/jobfile.txt') as jobfile:
+        jobfile.writelines([name + ', ' + str(job) for name, job in jobs.items()])
