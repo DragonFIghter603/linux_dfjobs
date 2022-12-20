1a9151fb9a7d9771b422922ca096e532ac58bffb
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2022-12-20 20:38:27 +0100

    Update dfjobs.py

---
+++ dfjobs.py
@@ -0,0 +1,35 @@
+import sys
+import argparse
+
+from commands.list_jobs import list_jobs
+from commands.add_job import add_job
+
+parser = argparse.ArgumentParser(prog='DfJobs')
+subparsers = parser.add_subparsers(dest='subcommand')
+
+def argument(*name_or_flags, **kwargs):
+    return ([*name_or_flags], kwargs)
+
+def subcommand(args=[], parent=subparsers):
+    def decorator(func):
+        parser = parent.add_parser(func.__name__, description=func.__doc__)
+        for arg in args:
+            parser.add_argument(*arg[0], **arg[1])
+        parser.set_defaults(func=func)
+    return decorator
+
+@subcommand()
+def list_jobs(args):
+    list_jobs()
+
+@subcommand([argument("name", help="add a job with name and path")])
+def add_job(args):
+    add_job(args.name, args.path)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    if args.subcommand is None:
+        parser.print_help()
+    else:
+        args.func(args)
