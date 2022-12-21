6d655485874daa53b9d77f8baaead63023992188
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2022-12-21 12:38:33 +0100

    Update list_jobs.py

--- commands/list_jobs.py
+++ commands/list_jobs.py
@@ -1,7 +1,8 @@
+from commands.run_tracked import is_running
 from util import load_jobs
 
 
 def list_jobs():
     jobs = load_jobs()
     for name, job in jobs.items():
-        print(f'{name:16} | {" ENABLED" if job.enabled else "DISABLED"} | {job.path} {job.command}')
+        print(f'{name:16} | {" ENABLED" if job.enabled else "DISABLED"} | {"STOPPED" if is_running(name) else "RUNNING"} | {job.path} {job.command}')
